8043c7f27e4eb8cf1135929531b37b02144045ac
szww33427 <847476048@qq.com>
2019-06-12 21:13:48 +0800

    绘制board时，加上框线

--- src/main/java/ui/BoardDisplayPanel.java
+++ src/main/java/ui/BoardDisplayPanel.java
@@ -16,6 +16,7 @@ public class BoardDisplayPanel extends JPanel {
 
     private static final Color DEAD_CELL_COLOR = Color.white;
     private static final Color LIVING_CELL_COLOR = Color.black;
+    private static final Color GRID_COLOR = Color.GRAY;
 
     @Nullable
     private volatile BufferedImage image;
@@ -50,5 +51,31 @@ public class BoardDisplayPanel extends JPanel {
                 new Dimension(getWidth(), getHeight())
         );
         g.drawImage(image, displayArea.x, displayArea.y, displayArea.width, displayArea.height, null);
+        g.setColor(GRID_COLOR);
+        drawGrid(g, displayArea);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void drawGrid(Graphics g, Rectangle displayArea) {
+        assert image != null;
+
+        for (int column = 0; column < image.getWidth(); column++) {
+            drawVerticalLine(g, displayArea, displayArea.x + displayArea.width * column / image.getWidth());
+        }
+        drawVerticalLine(g, displayArea, displayArea.x + displayArea.width - 1);
+
+        /* 绘制水平线*/
+        for (int row = 0; row < image.getHeight(); row++) {
+            drawHorizontalLine(g, displayArea, displayArea.y + displayArea.height * row / image.getHeight());
+        }
+        drawHorizontalLine(g, displayArea, displayArea.y + displayArea.height - 1);
+    }
+
+    private void drawHorizontalLine(Graphics g, Rectangle displayArea, int y) {
+        g.drawLine(displayArea.x, y, displayArea.x + displayArea.width, y);
+    }
+
+    private void drawVerticalLine(Graphics g, Rectangle displayArea, int x) {
+        g.drawLine(x, displayArea.y, x, displayArea.y + displayArea.height);
     }
 }
--- src/main/java/ui/load/SelectPanel.java
+++ src/main/java/ui/load/SelectPanel.java
@@ -18,18 +18,22 @@ class SelectPanel extends JPanel {
     SelectPanel(List<BoardItem> items) {
         this.items = new ArrayList<>(items);
 
-        setLayout(new GridLayout(BOARD_COUNT_PER_ROW, BOARD_COUNT_PER_PAGE / BOARD_COUNT_PER_ROW));
+        setLayout(new GridLayout(BOARD_COUNT_PER_ROW, BOARD_COUNT_PER_PAGE / BOARD_COUNT_PER_ROW, 5, 5));
         boardItemPanels = new BoardItemPanel[BOARD_COUNT_PER_PAGE];
         for (int i = 0; i < BOARD_COUNT_PER_PAGE; i++) {
             boardItemPanels[i] = new BoardItemPanel();
             add(boardItemPanels[i]);
         }
 
+        display();
+    }
+
+    private void display() {
         // offset：当前页面所显示的第几张图像
         for (int offset = 0; offset < BOARD_COUNT_PER_PAGE; offset++) {
             int index = startIndex + offset;
-            if (index < this.items.size()) {
-                boardItemPanels[offset].set(this.items.get(index));
+            if (index < items.size()) {
+                boardItemPanels[offset].set(items.get(index));
             } else {
                 boardItemPanels[offset].set(null);
             }
